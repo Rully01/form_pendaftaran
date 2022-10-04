691fd2c020e608112e5409ecc127e219de96079f
Rully01 <109121699+Rully01@users.noreply.github.com>
2022-10-04 15:17:20 +0700

    Add files via upload

---
+++ proses-pendaftaran.php
@@ -0,0 +1,22 @@
+<?php
+include("koneksi.php");
+
+if(isset($_POST['daftar'])){
+    $nama = $_POST['nama'];
+    $tempat = $_POST['tempat'];
+    $tanggal = $_POST['tanggal'];
+    $alamat = $_POST['alamat'];
+    $jk = $_POST['jk'];
+    $agama= $_POST['agama'];
+
+    $sql = "INSERT INTO tb_daftar (nama, tempat, tanggal, alamat, jk, agama) VALUES ('$nama', '$tempat', '$tanggal', '$alamat', '$jk', '$agama')";
+	$query = mysqli_query($db, $sql);
+
+    if( $query ) {
+		header('Location: tabel-daftar.php?status=sukses');
+	} else {
+		header('Location: form-pendaftaran.php?status=gagal');
+	}
+}
+?>
+<!-- Nama : wahyu rizky nh  -->
